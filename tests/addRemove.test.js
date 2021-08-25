5a1b3da84da7a43fdbd1b378fbbcc3a1354a37db
Igor.Koloski <koloski_igor@hotmail.com>
2021-08-25 12:55:46 +0200

    Add tests for removing elements

--- tests/addRemove.test.js
+++ tests/addRemove.test.js
@@ -1,10 +1,10 @@
+import { JSDOM } from 'jsdom';
 import ToDo from '../src/module/todo.js';
-import {createLiTodo} from '../src/module/utilities.js';
+import { createLiTodo } from '../src/module/utilities.js';
 import Storage from '../src/module/localStorage.js';
 
-import { JSDOM } from "jsdom";
-const dom = new JSDOM()
-global.document = dom.window.document
+const dom = new JSDOM();
+global.document = dom.window.document;
 
 class LocalStorageMock {
   constructor() {
@@ -58,6 +58,23 @@ describe('on page load', () => {
 const newToDo = new ToDo('NewTodo');
 const newArray = [newToDo];
 localStorage.setItem('newProjectList', JSON.stringify(newArray));
+Storage.set([
+  {
+    index: 0,
+    description: 'CapstoneProject',
+    checked: false,
+  },
+  {
+    index: 1,
+    description: 'Todo Project',
+    checked: false,
+  },
+  {
+    index: 2,
+    description: 'RestaurauntProject',
+    checked: false,
+  },
+]);
 
 describe('after adding new element', () => {
   it('it should add new element in a new project list', () => {
@@ -69,28 +86,10 @@ describe('after adding new element', () => {
     expect(storage.length).toEqual(1);
   });
 
-  test('Add one new item to the list', () => {
-    document.body.innerHTML =
-    '<div>' +
-    '  <ul id="list"></ul>' +
-    '</div>';
-    Storage.set([
-      {
-        index: 0,
-        description: 'CapstoneProject',
-        checked: false
-      },
-      {
-        index: 1,
-        description: 'Todo Project',
-        checked: false
-      },
-      {
-        index: 2,
-        description: 'RestaurauntProject',
-        checked: false
-      },
-    ]);
+  it('Add one new item to the list', () => {
+    document.body.innerHTML = '<div>'
+    + '  <ul id="list"></ul>'
+    + '</div>';
     const list = document.getElementById('list');
     let index = -1;
     const storage = Storage.get();
@@ -100,8 +99,50 @@ describe('after adding new element', () => {
       elem.index = index;
       // Create li, check box and description h5 for every Todo Object
       const newLi = createLiTodo(elem.description, index);
-      list.appendChild( newLi);
+      list.appendChild(newLi);
     });
     expect(list.children).toHaveLength(3);
+  });
 });
+
+describe('after removing element', () => {
+  it('should remove element from the list', () => {
+    document.body.innerHTML = '<div>'
+    + '  <ul id="list"></ul>'
+    + '</div>';
+    const list = document.getElementById('list');
+    let index = -1;
+    const storage = Storage.get();
+    storage.forEach((elem) => {
+      // Reset the indexes starting from 1
+      index += 1;
+      elem.index = index;
+      // Create li, check box and description h5 for every Todo Object
+      const newLi = createLiTodo(elem.description, index);
+      list.appendChild(newLi);
+    });
+    const button = list.childNodes[0].childNodes[3];
+    button.click();
+    expect(list.childNodes).toHaveLength(2);
+  });
+  it('should remove item from local storage', () => {
+    document.body.innerHTML = '<div>'
+    + '  <ul id="list"></ul>'
+    + '</div>';
+    const list = document.getElementById('list');
+    let index = -1;
+    const storage = Storage.get();
+    storage.forEach((elem) => {
+      // Reset the indexes starting from 1
+      index += 1;
+      elem.index = index;
+      // Create li, check box and description h5 for every Todo Object
+      const newLi = createLiTodo(elem.description, index);
+      list.appendChild(newLi);
+    });
+    const button = list.childNodes[0].childNodes[3];
+    button.click();
+    Storage.set(storage);
+    expect(Storage.get()).toHaveLength(2);
+  });
 });
